8e2826a4ff422d713344dd8128327f3ec81c3c7c
danaasujfj <99951392+danaasujfj@users.noreply.github.com>
2022-03-04 08:45:31 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onPinPressed(TouchPin.P0, function () {
     basic.showNumber(randint(0, 100))
 })
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("" + (Math.randomBoolean()))
+})
 basic.forever(function () {
     basic.showLeds(`
         . # . # .
