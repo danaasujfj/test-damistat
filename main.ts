4cc0f307330c2dfb6e928f412e8434c0358575af
danaasujfj <99951392+danaasujfj@users.noreply.github.com>
2022-03-04 08:43:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(randint(0, 100))
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+})
